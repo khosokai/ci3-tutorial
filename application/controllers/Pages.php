bbd66c79b9b5af8789625e6adc60be06a1a0d067
Kenta Hosokai <hosokai.kenta@gmail.com>
2019-09-09 22:58:52 +0900

    codeigniter3 tutorial

---
+++ application/controllers/Pages.php
@@ -0,0 +1,23 @@
+<?php
+class Pages extends CI_Controller {
+
+    /**
+     * view function
+     *
+     * @param string $page
+     * @return void
+     */
+    public function view($page = 'home')
+    {
+        if (!file_exists(APPPATH.'views/pages/'.$page.'.php')) {
+            // おっと、そのページはありません！
+            show_404();
+        }
+
+        $data['title'] = ucfirst($page); // 頭文字を大文字に
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('pages/'.$page, $data);
+        $this->load->view('templates/footer', $data);
+    }
+}
